125ea6028a40dfd454f12d9464ae3ca39c23e816
Adrien Cappelaere <47740396+AdrienCappelaere@users.noreply.github.com>
2025-01-16 08:48:47 +0100

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Welcome to SERP Speed Checker</h1>
+      <p>Start building your app here!</p>
+    </div>
+  );
+}
+
+export default HomePage;
